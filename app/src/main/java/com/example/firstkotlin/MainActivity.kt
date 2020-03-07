9fc69f334dddf0a1cda945fb89f0a874a6b64c61
Zxongfu <sonis123000@gmail.com>
2020-03-07 22:30:56 +0800

    First kotlin

---
+++ app/src/main/java/com/example/firstkotlin/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.firstkotlin
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button:Button =findViewById(R.id.button)
+
+        button.setOnClickListener{
+            Toast.makeText(this@MainActivity,"OK",Toast.LENGTH_LONG).show()
+        }
+    }
+}
